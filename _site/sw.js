2f6a0b9f87ab91af37d15ce2164627375b437c37
iamjjchang <beyondsjj@gmail.com>
2023-09-22 21:40:19 +0800

    initial commit12

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230922.211904';
+const cacheName = 'chirpy-20230922.213505';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
